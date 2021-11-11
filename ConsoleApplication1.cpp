f43d72511e700f915abec3281b0e6c181dc2f4f5
AndrewGalynin <galynin69@gmail.com>
2021-11-11 16:10:21 +0200

    delete do while

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -24,14 +24,7 @@ int main()
 
 	cout << "do...while" << endl;
 	x = min;
-	// DO WHILE
-	do
-	{
-		y = pow(x, 3) + pow(x, 1 / 2) - 3 * c;
-		cout << setw(10) << "x= " << x << setw(10) << "y= " << y << endl;
-		x += h;
-	}
-	while (x <= max + h / 2);
+	
 }
 
 
